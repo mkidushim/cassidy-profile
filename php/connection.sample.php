820133f7768eed9107134f40560bf28d2278d893
mkidushim <mkidushim@gmail.com>
2017-08-30 20:36:03 -0700

    connection sample

--- php/connection.sample.php
+++ php/connection.sample.php
@@ -3,37 +3,21 @@
 	header("Content-Type: application/json");
 	// $androidApiKey = '';
 
-
-	$dbHost = '';
-	$dbUser = '';
-	$dbPass = '';
-	$dbName = '';
-
-	$emailHost = '';
+  $emailHost = 'smtp.gmail.com';
+	$emailPass = '';
 	$fromEmail = '';
-
+	$fromEmailName = 'Cassidy Humphrey Website';
+	
 	$response = array(
 		'status'=>'NO',
 		'content'=>'',
 	);
-
-	$m = new mysqli($dbHost,$dbUser,$dbPass,$dbName);
-	if ($m->connect_errno){
-		$response['content'] = 'Error connectiong to the database.';
-		log_and_respond($response);
-		exit;
-	}
-	
   function cleanInput($v){
-    global $m;
     $v = stripslashes($v);
-    $v = $m->real_escape_string($v);
     $v = trim($v);
     return $v;
   } 
   function log_and_respond($response){
-    global $m;    
-    global $_REQUEST;
     $resp = json_encode($response);
     echo $resp;
     exit;
