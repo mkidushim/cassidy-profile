61f331eccdd9cab74136218f5778312ea4c89c7f
mkidushim <mkidushim@gmail.com>
2017-09-17 09:37:46 -0700

    new page transitions

--- header.php
+++ header.php
@@ -10,13 +10,13 @@
       <a class="navbar-brand text-bold text-xxl" href="index.php">Cassidy <span class="color-name">Humphrey</span></a>
     </div>
     <div id="navbar" class="navbar-collapse collapse">
-      <ul class="nav navbar-nav navbar-right">
-        <li class="home active" target="home"><a>Home</a></li>
-        <li class="experience" target="experience"><a>Experience</a></li>
-        <li class="education" target="education"><a>Education</a></li>
-        <li class="skills" target="skills"><a>Skills</a></li>
-        <li class="interests" target="interests"><a>Interests</a></li>
-        <li class="contact" target="contact"><a>Contact</a></li>
+      <ul class="nav navbar-nav navbar-right site-main-menu">
+        <li class="home active" target="home"><a class="pt-trigger" href="#home">Home</a></li>
+        <li class="experience" target="experience"><a class="pt-trigger" href="#experience">Experience</a></li>
+        <li class="education" target="education"><a class="pt-trigger" href="#education">Education</a></li>
+        <li class="skills" target="skills"><a class="pt-trigger" href="#skills">Skills</a></li>
+        <li class="interests" target="interests"><a class="pt-trigger" href="#interests">Interests</a></li>
+        <li class="contact" target="contact"><a class="pt-trigger" href="#contact">Contact</a></li>
       </ul>
     </div>
   </div>
